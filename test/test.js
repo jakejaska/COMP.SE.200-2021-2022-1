18e954aaa16d26bcd2cc0f408dc6ac18500d55a5
test <94987642+jakejaska@users.noreply.github.com>
2021-11-26 01:01:41 +0200

    c8, mocha, chai to get started

---
+++ test/test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai'
+import divide from '../src/divide.js'
+
+
+describe('Divide.js tests', () => {
+  it('divide 6 by 3 should be 2', () => {
+    const ans = divide(6, 3)
+    expect(ans).to.equal(2)
+  })
+  it('division by 0', () => {
+    expect(() => divide(2,0)).to.throw(RangeError)
+  })
+  it('uneven', () => {
+    const ans = divide(-4, 2)
+    expect(ans).to.eq(-2)
+  })
+  it('number of arguments', () => {
+    //expect(() => divide(1)).to.throw(TypeError)
+    expect(() => divide(1,2,3)).to.throw(TypeError)
+    expect(() => divide()).to.throw(TypeError)
+  })
+  it('incorrect parameter type', () => {
+    expect(() => divide("asd", 1)).to.throw(TypeError)
+  })
+})
